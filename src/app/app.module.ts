58c0399cb52d0db93ba7546f55c3624d1a1436d9
Aviking88 <avinash.gic@gmail.com>
2019-06-08 10:55:06 +0530

    5 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import {MaterialModulesModule} from './core/material-modules/material-modules.module'
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component'
 import { HelloComponent } from './hello.component';
 
+const AppRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent
+
+  }];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule,BrowserAnimationsModule,
+  RouterModule.forRoot(AppRoutes), FormsModule,MaterialModulesModule ],
+  declarations: [ AppComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
