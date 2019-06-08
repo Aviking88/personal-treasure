323ca0705b19c35544c8bcb900a3bf5308baece8
Aviking88 <avinash.gic@gmail.com>
2019-06-08 12:03:47 +0530

    Added support for nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-import {MaterialModulesModule} from './core/material-modules/material-modules.module'
+import { MaterialModulesModule } from './core/material-modules/material-modules.module'
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from './login/login.component'
+import { LoginComponent } from './login/login.component'
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { NbSidebarModule, NbLayoutModule, NbButtonModule, NbThemeModule } from '@nebular/theme';
 
 const AppRoutes: Routes = [
   {
@@ -22,11 +24,19 @@ const AppRoutes: Routes = [
     pathMatch: 'full',
     component: LoginComponent
 
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent
+
   }];
 @NgModule({
-  imports:      [ BrowserModule,BrowserAnimationsModule,
-  RouterModule.forRoot(AppRoutes), FormsModule,MaterialModulesModule ],
-  declarations: [ AppComponent, LoginComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes), NbThemeModule.forRoot(),NbLayoutModule,
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule, FormsModule, MaterialModulesModule],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
